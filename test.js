566e141431b2fd65fe51b1dec08a1831dc250257
mohammadfarheen <46694155+mohammadfarheen@users.noreply.github.com>
2019-02-22 21:03:30 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,33 @@
-function circumference(input1){
-    radius=input1;
-    let CircumferenceOfCircle;
-        if(radius<0){
-   CircumferenceOfCircle=0;
 
-    }
-        else if(radius>250){
-        CircumferenceOfCircle=0;
 
-     }
-        else{
-        CircumferenceOfCircle=2*(3.14)*(radius)
-    }
-   return CircumferenceOfCircle;
 
-}
+QUnit.test("TestCircle",function(assert)
+{
+    var res=area(5);
+    assert.equal(res, "78.5" ,"area of circle is  78.5");
+});
+
+QUnit.test("TestCircle",function(assert)
+{
+    var res=area(6);
+    assert.equal(res, "113.03999999999999" ,"area of circle is  113.03999999999999");
+
+});
+
+QUnit.test("TestCircle",function(assert)
+{
+    var res=area(3);
+    assert.equal(res, "28.259999999999998" ,"area of circle is  28.259999999999998");
+});
+
+QUnit.test("TestCircle",function(assert)
+{
+    var res=area(7);
+    assert.equal(res, "153.86" ,"area of circle is  153.86");
+});
+
+QUnit.test ("TestCircle",function(assert)
+{
+    var res=area(8);
+    assert.equal(res, "200.96" ,"area of circle is  200.96");
+});
