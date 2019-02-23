3a25edb10714b5d5fb770f55e92a75b304da91c7
mohammadfarheen <46694155+mohammadfarheen@users.noreply.github.com>
2019-02-22 21:02:19 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-cv
+function circumference(input1){
+    radius=input1;
+    let CircumferenceOfCircle;
+        if(radius<0){
+   CircumferenceOfCircle=0;
+
+    }
+        else if(radius>250){
+        CircumferenceOfCircle=0;
+
+     }
+        else{
+        CircumferenceOfCircle=2*(3.14)*(radius)
+    }
+   return CircumferenceOfCircle;
+
+}
